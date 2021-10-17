40fe03bd75df2d9f96bcc332c2c9e78257bfa518
kukli <72162161+spanishkukli@users.noreply.github.com>
2021-10-17 16:38:25 +0200

    Update files

---
+++ logic-gates.h
@@ -0,0 +1,10 @@
+#pragma once  
+
+// Forward declaration
+bool _not(bool);
+bool _and(bool, bool);
+bool _or(bool, bool);
+bool _xor(bool, bool);
+bool _nand(bool, bool);
+bool _nor(bool, bool);
+bool _xnor(bool, bool);
